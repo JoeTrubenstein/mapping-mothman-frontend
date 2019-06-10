5bcd78bfcb303f915af8e204202bfa970b53a1bd
Joseph Trubenstein <josephtrubenstein@gmail.com>
2019-06-10 18:12:23 -0400

    added jwt auth to admin dash

--- src/Components/AdminDash.js
+++ src/Components/AdminDash.js
@@ -1,41 +1,76 @@
 import React from "react";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+import setAuthJWT from "../utils/setAuthJWT";
+
 class AdminDash extends React.Component {
+  
+  // declare starting state
   state = {
+    isAuth: false,
     marker: {},
     sightings: [],
-    jwt: "token goes here"
+    decoded: {},
+    test: "hello"
   };
 
   componentWillMount() {
-    this.getSightings()
+    this.getSightings();
   }
 
-  login = () => {
+  // populate the state with the input values on change
+  loginValues = event => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value
+      },
+      () => {
+      }
+    );
+  };
+
+  // stop page from clearing the state prematurely
+  login = event => {
+    event.preventDefault();
+
+    // get the login params from the state
     let config = {
-      email: "mothman",
-      password: "mothman"
+      email: this.state.email,
+      password: this.state.pw
     };
+
     axios
       .post("https://mothman-server.herokuapp.com/users/signin", config)
-
       .then(res => {
-        console.log(res.data);
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        const decoded = jwt_decode(token);
+        setAuthJWT(token);
+        this.setState({
+          // add the decoded JWT to the state
+          decoded: decoded,
+          // toggle the auth boolean
+          isAuth: true
+        });
       })
       .catch(error => {
         console.log(error);
       });
   };
 
-  getSightings = () => {
-    let config = {
-      headers: {
-        Authorization: this.state.jwt
-      }
-    };
+  // destroy the token in local storage, remove decoded token from state, and toggle the auth boolean
+  logout = () => {
+    this.setState({
+      decoded: {},
+      isAuth: false
+    });
+    localStorage.removeItem("jwtToken");
+  };
 
+  // retrieve all the sightings from the MLAB DB
+  getSightings = () => {
     axios
-      .get("https://mothman-server.herokuapp.com/users/get-sightings", config)
+      .get("https://mothman-server.herokuapp.com/users/get-sightings")
       .then(res => {
         let items = res.data;
 
@@ -59,7 +94,6 @@ class AdminDash extends React.Component {
             },
             () => {}
           );
-          console.log(this.state);
         });
       })
       .catch(error => {
@@ -67,6 +101,7 @@ class AdminDash extends React.Component {
       });
   };
 
+  // map the retrieved sightings
   showSighting = () => {
     return this.state.sightings.map(sightings => {
       return (
@@ -104,17 +139,59 @@ class AdminDash extends React.Component {
   };
 
   render() {
+    // retrieve the auth boolean from the state
+    const { isAuth } = this.state;
     return (
       <div>
-        <h1>Admin Dashboard</h1>
-        <form>
-          <input></input>
+        <form className="form-inline my-2 my-lg-0" onSubmit={this.login}>
+          <input
+            className="form-control mr-sm-2"
+            type="text"
+            name="email"
+            placeholder="email"
+            aria-label="userName"
+            // run the value functions so these fields can be saved in the state, then used in the POST request
+            onChange={this.loginValues}
+            value={this.state.username}
+          />
+          <input
+            className="form-control mr-sm-2"
+            type="password"
+            name="pw"
+            placeholder="Password"
+            aria-label="Password"
+            onChange={this.loginValues}
+            value={this.state.password}
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Login
+          </button>
         </form>
-        <div className="container-fluid">
-          <div className="card-deck mb-3 text-center">
-            {this.showSighting()}
+        <button
+          className="btn btn-outline-success my-2 my-sm-0"
+          onClick={this.logout}
+        >
+          Logout
+        </button>
+        <h1>Admin Dashboard</h1>
+
+      {/* Check the auth boolean to see what will be displayed */}
+      
+        {isAuth ? (
+
+          <div>
+            <h3>Logged in as {this.state.decoded.email}</h3>
+            <div className="card-deck mb-3 text-center">
+              {this.showSighting()}
+            </div>
           </div>
-        </div>
+        ) : (
+          <h1>Log in to approve sightings</h1>
+        )}
+        <div className="container-fluid" />
       </div>
     );
   }
