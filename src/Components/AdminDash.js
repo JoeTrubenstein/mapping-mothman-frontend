e4901ec74987268220ed11fbea1c66bc21a9593a
Joseph Trubenstein <josephtrubenstein@gmail.com>
2019-06-12 17:25:10 -0400

    debugging

--- src/Components/AdminDash.js
+++ src/Components/AdminDash.js
@@ -56,8 +56,7 @@ class AdminDash extends React.Component {
     };
 
     axios
-      // .post("https://mothman-server.herokuapp.com/users/signin", config)
-      .post("https://mothman-server.herokuapp.com/users/signin", config)
+      .post("https://mothman-server.herokuapp.com/users/admin-signin", config)
       .then(res => {
         const { token } = res.data;
         localStorage.setItem("jwtToken", token);
